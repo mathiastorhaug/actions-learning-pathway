8ca3507590d5aa6a73df68e6d8add00c856b6113
Mathias Tørhaug <141143243+mathiastorhaug@users.noreply.github.com>
2025-10-24 09:28:26 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,19 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
 export default function Home() {
   return (
     <>
